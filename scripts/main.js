2a1ab4377ab4d7bb16ec38abc833779924048adb
CheoR <cheo.roman+github@gmail.com>
2021-01-22 17:56:56 -0600

    Render components to index.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 // import { getOfficers } from "./officers/OfficerProvider.js"
 
 // const officers = getOfficers()
+
+// import { useCriminals } from "./criminal/CriminalDataProvider.js"
+// import { getCriminals } from "./criminal/CriminalDataProvider.js"
+
+// getCriminals()
+// let criminals = useCriminals()
+// import { CriminalList } from "./criminals/CriminalList.js"
+
+// CriminalList();
+
+import { OfficerList } from "./officers/OfficerList.js"
+
+OfficerList();
