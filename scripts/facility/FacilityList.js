ae814dc0a4ffce1f1fb065d8fdea7b5abd4b2e0d
CheoR <cheo.roman+github@gmail.com>
2021-02-12 15:06:04 -0600

    feat - add facilities components

---
+++ scripts/facility/FacilityList.js
@@ -0,0 +1,9 @@
+
+
+
+/*
+
+The criminal list should listen for that event and toggle its visibility.
+Create a Facility component and a FacilityList component.
+Create /styles/facilities.css file and copy/pasta the code from statements.css and change the colors.
+*/
